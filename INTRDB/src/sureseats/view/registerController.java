cc07c6a6ec78dedd07981181e8383a26be5ee301
diane1998 <diane_cruz@dlsu.edu.ph>
2017-11-30 19:03:47 +0800

    switchable scenes

--- INTRDB/src/sureseats/view/registerController.java
+++ INTRDB/src/sureseats/view/registerController.java
@@ -1,5 +1,16 @@
+package sureseats.view;
+
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
@@ -58,5 +69,18 @@ public class registerController {
 
     @FXML
     private ChoiceBox<?> rp3;
+    
+    public void goMain(ActionEvent event) throws IOException
+    {
+ 	   FXMLLoader loader = new FXMLLoader();
+ 	   loader.setLocation(getClass().getResource("/sureseats/view/GUI.fxml"));
+        Parent tableViewParent = loader.load(); 
+        Scene tableViewScene = new Scene(tableViewParent);
+        //This line gets the Stage information
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        
+        window.setScene(tableViewScene);
+        window.show();
+    }
 
 }
---
+++ INTRDB/src/sureseats/view/scheduleController.java
@@ -0,0 +1,41 @@
+package sureseats.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+public class scheduleController {
+
+    @FXML
+    private Button Sback;
+
+    @FXML
+    private Text STitle;
+
+    @FXML
+    private ImageView SMovie;
+
+    @FXML
+    private Text SDate;
+
+    @FXML
+    private TextArea SSched1;
+
+    @FXML
+    private TextArea SSched2;
+
+    @FXML
+    private TextArea SSched3;
+
+    @FXML
+    private Button SReserve1;
+
+    @FXML
+    private Button SReserve2;
+
+    @FXML
+    private Button SReseve3;
+
+}
