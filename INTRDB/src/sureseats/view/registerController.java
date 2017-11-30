4b7804c63018f2497f78637d9f85fe43350e133a
Luis Lopez <luis_lopez@dlsu.edu.ph>
2017-12-01 02:04:40 +0800

    Register screen choice boxes now display choices

--- INTRDB/src/sureseats/view/registerController.java
+++ INTRDB/src/sureseats/view/registerController.java
@@ -3,8 +3,10 @@ package sureseats.view;
 import javafx.scene.control.TextField;
 import java.time.LocalDate;
 
-
 import javafx.stage.Stage;
+import sureseats.model.CityService;
+import sureseats.model.MallService;
+import sureseats.model.ProvinceService;
 import sureseats.model.User;
 
 import java.io.IOException;
@@ -25,158 +27,160 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.PasswordField;
 
+import sureseats.model.*;
+
 public class registerController {
-	
-	
+
 	// here is the declaration of list of strings for the choice boxes
-	//ObservableList<String> provinceList= FXCollections.observableArrayList(ENTER LIST OF STRINGS)
-	//ObservableList<String> cityList= FXCollections.observableArrayList(ENTER LIST OF STRINGS)
-	//ObservableList<String> preferredList= FXCollections.observableArrayList(ENTER LIST OF STRINGS)
-	
+	// ObservableList<String> provinceList= FXCollections.observableArrayList({})
+	// ObservableList<String> cityList= FXCollections.observableArrayList({})
+	// ObservableList<String> preferredList= FXCollections.observableArrayList({})
+
 	private User user;
 
-    @FXML
-    private Button rRegister;
+	@FXML
+	private Button rRegister;
 
-    @FXML
-    private Button rback;
+	@FXML
+	private Button rback;
 
-    @FXML
-    private TextField rUsername;
+	@FXML
+	private TextField rUsername;
 
-    @FXML
-    private TextField rEmail;
+	@FXML
+	private TextField rEmail;
 
-    @FXML
-    private TextField rMobile;
+	@FXML
+	private TextField rMobile;
 
-    @FXML
-    private TextField rFName;
+	@FXML
+	private TextField rFName;
 
-    @FXML
-    private TextField rLName;
+	@FXML
+	private TextField rLName;
 
-    @FXML
-    private PasswordField rPassword;
+	@FXML
+	private PasswordField rPassword;
 
-    @FXML
-    private PasswordField rVPassword;
+	@FXML
+	private PasswordField rVPassword;
 
-    @FXML
-    private ChoiceBox<?> rp1;
+	@FXML
+	private ChoiceBox<Mall> rp1;
 
-    @FXML
-    private ChoiceBox<?> rCity;
+	@FXML
+	private ChoiceBox<City> rCity;
 
-    @FXML
-    private ChoiceBox<?> rProvince;
+	@FXML
+	private ChoiceBox<Province> rProvince;
 
-    @FXML
-    private CheckBox rCBFemale;
+	@FXML
+	private CheckBox rCBFemale;
 
-    @FXML
-    private CheckBox rCBMale;
+	@FXML
+	private CheckBox rCBMale;
 
-    @FXML
-    private DatePicker rBirthdate;
+	@FXML
+	private DatePicker rBirthdate;
 
-    @FXML
-    private ChoiceBox<?> rp2;
+	@FXML
+	private ChoiceBox<Mall> rp2;
 
-    @FXML
-    private ChoiceBox<?> rp3;
-    
-   @FXML
-    private void handleFemale()
-    {
-    	if(rCBFemale.isSelected())
-    		 rCBMale.setSelected(false);
-    }
-    
-    @FXML
-    private void handleMale()
-    {
-    	if( rCBMale.isSelected())
-    		rCBFemale.setSelected(false);
-    }
-    
-    public void goMain(ActionEvent event) throws IOException
-    {
- 	   FXMLLoader loader = new FXMLLoader();
- 	   loader.setLocation(getClass().getResource("/sureseats/view/GUI.fxml"));
-        Parent tableViewParent = loader.load(); 
-        Scene tableViewScene = new Scene(tableViewParent);
-        //This line gets the Stage information
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-        
-        window.setScene(tableViewScene);
-        window.show();
-    }
-    
-    public void confirmRegister(ActionEvent event) throws IOException
-    {
-    	
-    	
-    	/*SureseatsDB sureseatsDB = new SureseatsDB();
-    	ProvinceService pservice = new ProvinceService(sureseatsDB);
-    	List<Province> listOfProvinces = pservice.getAll()
-    			*/
-    	if(rPassword.getText().equals(rVPassword.getText())&& rEmail.getText().equals("")==false && rFName.getText().equals("")==false && rLName.getText().equals("")==false && rUsername.getText().equals("")==false &&  rMobile.getText().equals("")==false &&(rCBFemale.isSelected() ||rCBMale.isSelected())&&rBirthdate.getValue()!=null )  
-    	{
-    		user= new User();
-    		user.setEmail(rEmail.getText());
-    		user.setFirstname(rFName.getText());
-    		user.setLastname(rLName.getText());
-    		user.setUsername(rUsername.getText());
-    		user.setMobileno(rMobile.getText());
-    		if(rCBFemale.isSelected())
-    			user.setGender("Female");
-    		else if(rCBMale.isSelected())
-    			user.setGender("Male");
-    		user.setBdate(rBirthdate.getValue());
-    		user.setIslocked(false);
-    		
-    		
-    		Alert alert = new Alert(AlertType.INFORMATION);
-    		alert.setTitle("Sign Up Complete!");
-    		alert.setHeaderText(" Registration confirmed ");
-    		alert.setContentText(" Registration is successful !");
+	@FXML
+	private ChoiceBox<Mall> rp3;
 
-    		alert.showAndWait();
-    		
-    		FXMLLoader loader = new FXMLLoader();
-    		loader.setLocation(getClass().getResource("/sureseats/view/GUI.fxml"));
-    		Parent tableViewParent = loader.load(); 
-    		Scene tableViewScene = new Scene(tableViewParent);
-    		//This line gets the Stage information
-    		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-    		window.setScene(tableViewScene);
-    		window.show();
-    	}
-    	else
-    	{
-    		Alert alert = new Alert(AlertType.ERROR);
-    		alert.setTitle("Error");
-    		alert.setHeaderText("Registration unsuccessful");
-    		alert.setContentText("Missing information or password does not match");
+	@FXML
+	private void handleFemale() {
+		if (rCBFemale.isSelected())
+			rCBMale.setSelected(false);
+	}
 
-    		alert.showAndWait();
-    	}
-    }
-    
-    @FXML
-    public void initialize()
-    {
-    	rProvince.setItems(provinceList);
-    	rCity.setItems(cityList);
-    	rp1.setItems(preferredList);
-    	rp2.setItems(preferredList);
-    	rp3.setItems(preferredList);
-    	
-    }
-    
-   
-    
+	@FXML
+	private void handleMale() {
+		if (rCBMale.isSelected())
+			rCBFemale.setSelected(false);
+	}
+
+	public void goMain(ActionEvent event) throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("/sureseats/view/GUI.fxml"));
+		Parent tableViewParent = loader.load();
+		Scene tableViewScene = new Scene(tableViewParent);
+		// This line gets the Stage information
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+		window.setScene(tableViewScene);
+		window.show();
+	}
+
+	public void confirmRegister(ActionEvent event) throws IOException {
+
+		/*
+		 * SureseatsDB sureseatsDB = new SureseatsDB(); ProvinceService pservice = new
+		 * ProvinceService(sureseatsDB); List<Province> listOfProvinces =
+		 * pservice.getAll()
+		 */
+		if (rPassword.getText().equals(rVPassword.getText()) && rEmail.getText().equals("") == false
+				&& rFName.getText().equals("") == false && rLName.getText().equals("") == false
+				&& rUsername.getText().equals("") == false && rMobile.getText().equals("") == false
+				&& (rCBFemale.isSelected() || rCBMale.isSelected()) && rBirthdate.getValue() != null) {
+			user = new User();
+			user.setEmail(rEmail.getText());
+			user.setFirstname(rFName.getText());
+			user.setLastname(rLName.getText());
+			user.setUsername(rUsername.getText());
+			user.setMobileno(rMobile.getText());
+			if (rCBFemale.isSelected())
+				user.setGender("Female");
+			else if (rCBMale.isSelected())
+				user.setGender("Male");
+			user.setBdate(rBirthdate.getValue());
+			user.setIslocked(false);
+
+			Alert alert = new Alert(AlertType.INFORMATION);
+			alert.setTitle("Sign Up Complete!");
+			alert.setHeaderText(" Registration confirmed ");
+			alert.setContentText(" Registration is successful !");
+
+			alert.showAndWait();
+
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("/sureseats/view/GUI.fxml"));
+			Parent tableViewParent = loader.load();
+			Scene tableViewScene = new Scene(tableViewParent);
+			// This line gets the Stage information
+			Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+			window.setScene(tableViewScene);
+			window.show();
+		} else {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Error");
+			alert.setHeaderText("Registration unsuccessful");
+			alert.setContentText("Missing information or password does not match");
+
+			alert.showAndWait();
+		}
+	}
+
+	@FXML
+	public void initialize() {
+		SureseatsDB sdb = new SureseatsDB();
+		ProvinceService ps = new ProvinceService(sdb);
+		CityService cs = new CityService(sdb);
+		UserService us = new UserService(sdb);
+		MallService ms = new MallService(sdb);
+		
+		ObservableList<Province> provinceList= FXCollections.observableArrayList(ps.getAll());
+		ObservableList<City> cityList= FXCollections.observableArrayList(cs.getAll());
+		ObservableList<Mall> mallList= FXCollections.observableArrayList(ms.getAll());
+		
+		rProvince.setItems(provinceList);
+		rCity.setItems(cityList);
+		rp1.setItems(mallList);
+		rp2.setItems(mallList);
+		rp3.setItems(mallList);
+
+	}
 
 	public User getUser() {
 		return user;
