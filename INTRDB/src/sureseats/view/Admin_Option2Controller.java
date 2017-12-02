37ade9962cb762be3edd14c1a595db0c4a12b4cc
diane1998 <diane_cruz@dlsu.edu.ph>
2017-12-03 01:10:49 +0800

    added admin

--- INTRDB/src/sureseats/view/Admin_Option2Controller.java
+++ INTRDB/src/sureseats/view/Admin_Option2Controller.java
@@ -1,10 +1,18 @@
 package sureseats.view;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class Admin_Option2Controller {
 
@@ -142,5 +150,38 @@ public class Admin_Option2Controller {
 
     @FXML
     private TextField RSID;
+
+    @FXML
+    private Button gotoAdminA;
+
+    @FXML
+    private Button Sign_In;
+    
+    public void toback(ActionEvent event) throws IOException
+    {
+ 	   FXMLLoader loader = new FXMLLoader();
+ 	   loader.setLocation(getClass().getResource("/sureseats/view/Admin_OptionsA.fxml"));
+        Parent tableViewParent = loader.load(); 
+        Scene tableViewScene = new Scene(tableViewParent);
+        //This line gets the Stage information
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        
+        window.setScene(tableViewScene);
+        window.show();
+    }
+    
+    public void ToSignIn(ActionEvent event) throws IOException
+    {
+ 	   FXMLLoader loader = new FXMLLoader();
+ 	   loader.setLocation(getClass().getResource("/sureseats/view/Sign_In.fxml"));
+        Parent tableViewParent = loader.load(); 
+        Scene tableViewScene = new Scene(tableViewParent);
+        //This line gets the Stage information
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        
+        window.setScene(tableViewScene);
+        window.show();
+    }
+    
 
 }
