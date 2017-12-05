e7ca2a870b227e143b4f7e24ab97a800fdc002ea
diane1998 <diane_cruz@dlsu.edu.ph>
2017-12-06 02:43:21 +0800

    Added java fx

--- INTRDB/src/sureseats/view/Admin_OptionsAController.java
+++ INTRDB/src/sureseats/view/Admin_OptionsAController.java
@@ -1,6 +1,10 @@
 package sureseats.view;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -19,6 +23,7 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+<<<<<<< HEAD
 import sureseats.model.Cinema;
 import sureseats.model.CinemaService;
 import sureseats.model.City;
@@ -118,28 +123,50 @@ public class Admin_OptionsAController {
 
     @FXML
     private TableColumn<City, Province> City_COL_PID;
+=======
+import sureseats.model.*;
 
-    @FXML
-    private TextField CCID;
+public class Admin_OptionsAController {
+	private SureseatsDB sdb;
+	private UserService us;
+	private ProvinceService ps;
+	private CityService cs;
 
-    @FXML
-    private Button CSearch;
+	@FXML
+	private Tab User_Malls;
 
-    @FXML
-    private Button CDelete;
+	@FXML
+	private TableView<User> Users_Table;
 
-    @FXML
-    private Button CUpdate;
+	@FXML
+	private TableView<Province> Province_Table;
 
-    @FXML
-    private TextField CCNo;
+	@FXML
+	private TableView<?> City_Table;
+>>>>>>> e1308bd16cd15d5d22fecfc3723c59563b53dc1c
 
-    @FXML
-    private TextField CCtype;
+	@FXML
+	private TextField CCID;
 
-    @FXML
-    private TextField CMID;
+	@FXML
+	private Button CSearch;
 
+	@FXML
+	private Button CDelete;
+
+	@FXML
+	private Button CUpdate;
+
+	@FXML
+	private TextField CCNo;
+
+	@FXML
+	private TextField CCtype;
+
+	@FXML
+	private TextField CMID;
+
+<<<<<<< HEAD
     @FXML
     private Button Cadd;
     
@@ -148,100 +175,113 @@ public class Admin_OptionsAController {
     
     @FXML
     private TextField MMID;
+=======
+	@FXML
+	private Button Cadd;
 
-    @FXML
-    private Button MSearch;
+	@FXML
+	private TextField MMID;
+>>>>>>> e1308bd16cd15d5d22fecfc3723c59563b53dc1c
 
-    @FXML
-    private Button MDelete;
+	@FXML
+	private Button MSearch;
 
-    @FXML
-    private Button MUpdate;
+	@FXML
+	private Button MDelete;
 
-    @FXML
-    private Button Madd;
+	@FXML
+	private Button MUpdate;
 
-    @FXML
-    private TextField MMType;
+	@FXML
+	private Button Madd;
 
-    @FXML
-    private TextField MCID;
+	@FXML
+	private TextField MMType;
 
-    @FXML
-    private TextField UUID;
+	@FXML
+	private TextField MCID;
 
-    @FXML
-    private Button USearch;
+	@FXML
+	private TextField UUID;
 
-    @FXML
-    private Button UDelete;
+	@FXML
+	private Button USearch;
 
-    @FXML
-    private Button Update;
+	@FXML
+	private Button UDelete;
 
-    @FXML
-    private TextField UUsername;
+	@FXML
+	private Button Update;
 
-    @FXML
-    private RadioButton UFemale;
+	@FXML
+	private TextField UUsername;
 
-    @FXML
-    private RadioButton Umale;
+	@FXML
+	private RadioButton UFemale;
 
-    @FXML
-    private RadioButton Ulocked;
+	@FXML
+	private RadioButton Umale;
 
-    @FXML
-    private RadioButton UUnlocked;
+	@FXML
+	private RadioButton Ulocked;
 
-    @FXML
-    private TextField UUEmail;
+	@FXML
+	private RadioButton UUnlocked;
 
-    @FXML
-    private TextField UUMobile;
+	@FXML
+	private TextField UUEmail;
 
-    @FXML
-    private TextField UUFirst;
+	@FXML
+	private TextField UUMobile;
 
-    @FXML
-    private TextField UUProvince;
+	@FXML
+	private TextField UUFirst;
 
-    @FXML
-    private TextField UUPassword;
+	@FXML
+	private TextField UUProvince;
 
-    @FXML
-    private TextField UUBdate;
+	@FXML
+	private TextField UUPassword;
 
-    @FXML
-    private TextField UULast;
+	@FXML
+	private TextField UUBdate;
 
-    @FXML
-    private Button UAdd;
+	@FXML
+	private TextField UULast;
 
+	@FXML
+	private Button UAdd;
+
+<<<<<<< HEAD
     @FXML
     private Button P_Load;
     
     @FXML
     private ChoiceBox<?> UCID;
+=======
+	@FXML
+	private ChoiceBox<?> UCID;
+>>>>>>> e1308bd16cd15d5d22fecfc3723c59563b53dc1c
 
-    @FXML
-    private ChoiceBox<?> UPID;
+	@FXML
+	private ChoiceBox<?> UPID;
 
-    @FXML
-    private TextField PPID;
+	@FXML
+	private TextField PPID;
 
-    @FXML
-    private Button PSearch;
+	@FXML
+	private Button PSearch;
 
-    @FXML
-    private Button PDelete;
+	@FXML
+	private Button PDelete;
 
-    @FXML
-    private Button PUpdate;
+	@FXML
+	private Button PUpdate;
 
-    @FXML
-    private Button PAdd;
+	@FXML
+	private Button PAdd;
 
+<<<<<<< HEAD
     @FXML
     private TextField PPName;
 
@@ -368,5 +408,116 @@ public class Admin_OptionsAController {
         window.setScene(tableViewScene);
         window.show();
     }
+=======
+	@FXML
+	private TextField PPName;
+
+	@FXML
+	private TextField CCTID;
+
+	@FXML
+	private Button CTSearch;
+
+	@FXML
+	private Button CTDelete;
+
+	@FXML
+	private Button CTUpdate;
+
+	@FXML
+	private Button CTAdd;
+
+	@FXML
+	private TextField CCTName;
+
+	@FXML
+	private TextField CCTType;
+
+	@FXML
+	private TextField CTPID;
+
+	@FXML
+	private Button admin1Next;
+
+	@FXML
+	private Button Back;
+	
+	/*@SuppressWarnings("unchecked")
+	public void initUserTable() {
+		ObservableList<User> allUsers = FXCollections.observableArrayList(us.getAll());
+		
+		
+
+		TableColumn<User, String> UUsernameCol = new TableColumn<User, String>("Username");
+		UUsernameCol.setCellValueFactory(new PropertyValueFactory<User, String>("username"));
+		
+		for 
+		
+		Users_Table.getColumns().setAll(UUsernameCol);
+
+		Users_Table.setItems(allUsers);
+	}*/
+	
+	@SuppressWarnings("unchecked")
+	public void initUserTable() {
+		ObservableList<User> allUsers = FXCollections.observableArrayList(us.getAll());
+		Users_Table.getColumns().setAll();
+		for (Field f : User.class.getDeclaredFields()) {
+			if (!Modifier.isFinal(f.getModifiers())) {
+				TableColumn<User, String> col = new TableColumn<User, String>(f.getName());
+				col.setCellValueFactory(new PropertyValueFactory<User, String>(f.getName()));
+				Users_Table.getColumns().add(col);
+			}
+		}
+		Users_Table.setItems(allUsers);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void initProvinceTable() {
+		ObservableList<Province> allProvinces = FXCollections.observableArrayList(ps.getAll());
+		Province_Table.getColumns().setAll();
+		for (Field f : Province.class.getDeclaredFields()) {
+			if (!Modifier.isFinal(f.getModifiers())) {
+				TableColumn<Province, String> col = new TableColumn<Province, String>(f.getName());
+				col.setCellValueFactory(new PropertyValueFactory<Province, String>(f.getName()));
+				Province_Table.getColumns().add(col);
+			}
+		}
+		Province_Table.setItems(allProvinces);
+	}
+
+	public void initialize() {
+		sdb = new SureseatsDB();
+		us = new UserService(sdb);
+		ps = new ProvinceService(sdb);
+		cs = new CityService(sdb);
+		initUserTable();
+		initProvinceTable();
+	}
+
+	public void toNext(ActionEvent event) throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("/sureseats/view/Admin_optionsB.fxml"));
+		Parent tableViewParent = loader.load();
+		Scene tableViewScene = new Scene(tableViewParent);
+		// This line gets the Stage information
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+		window.setScene(tableViewScene);
+		window.show();
+	}
+
+	public void ToSignIn(ActionEvent event) throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("/sureseats/view/Sign_In.fxml"));
+		Parent tableViewParent = loader.load();
+		Scene tableViewScene = new Scene(tableViewParent);
+		// This line gets the Stage information
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+		window.setScene(tableViewScene);
+		window.show();
+	}
+>>>>>>> e1308bd16cd15d5d22fecfc3723c59563b53dc1c
 
 }
