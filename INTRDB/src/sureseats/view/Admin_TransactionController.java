a1ec79188af87579b73968eb92d55eded03fd3ed
diane1998 <diane_cruz@dlsu.edu.ph>
2017-12-06 23:20:58 +0800

    may kulang pala

--- INTRDB/src/sureseats/view/Admin_TransactionController.java
+++ INTRDB/src/sureseats/view/Admin_TransactionController.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -46,7 +47,7 @@ public class Admin_TransactionController {
          Parent tableViewParent = loader.load(); 
          Scene tableViewScene = new Scene(tableViewParent);
          //This line gets the Stage information
-         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+         Stage window = (Stage)((Node) event.getSource()).getScene().getWindow();
          
          window.setScene(tableViewScene);
          window.show();
